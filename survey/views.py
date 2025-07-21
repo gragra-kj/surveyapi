5c7de24dedcc94f437adf5406136794ea8fbf066
gragra-kj <muthuigracia27@gmail.com>
2025-07-21 14:38:40 +0300

    second viewsets

--- survey/views.py
+++ survey/views.py
@@ -8,23 +8,28 @@ from rest_framework import permissions
 class SurveyViewSets(viewsets.ModelViewSet):
     queryset=SurveyModels.objects.all()
     serializer_class=SurveySerializer
-    permission_classes=[]
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
     
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset=QuestionModels.objects.all()
     serializer_class=QuestionSerializers
-    permission_classes=[]    
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]  
+      
 class ResponseViewSet(viewsets.ModelViewSet):
     queryset=ResponseModels.objects.all()
     serializer_class=ResponseSerializer
-    permission_classes=[]
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]
     
 class AnswerViewset(viewsets.ModelViewSet):
     queryset=AnswerModels.objects.all()
     serializer_class=AnswerSerializers 
-    permission_classes=[]   
+    permission_classes=[permissions.AllowAny]   
+    
+    def perform_create(self, serializer):
+        user = self.request.user if self.request.user.is_authenticated else None
+        serializer.save(user=user)
     
 class ChoicesViewSets(viewsets.ModelViewSet):
     queryset=Choice.objects.all()
     serializer_class=ChoiceSerializer
-    permission_classes=[]    
+    permission_classes=[permissions.IsAuthenticatedOrReadOnly]    
